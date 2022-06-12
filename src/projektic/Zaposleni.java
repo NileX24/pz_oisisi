c26c120f598ada518abf8d855493e6f7add6a056
Branko008 <balboarocky873@gmail.com>
2022-06-12 17:01:59 +0200

    Zaposleni
    Zaposleni

---
+++ src/projektic/Zaposleni.java
@@ -0,0 +1,10 @@
+package projektic;
+
+public class Zaposleni {
+	String broj,ulica,grad;
+	void unos(String broj, String ulica, String grad) {
+		this.broj=broj;
+		this.grad=grad;
+		this.ulica=ulica;
+	}
+}
