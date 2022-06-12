2dc1db028aeb318888e88d7fcb45bd7180d5156b
VukBabic <vukbabic1609@gmail.com>
2022-06-12 16:00:28 +0200

    Dodavanje Adrese
    Start

---
+++ src/projektic/Adresa.java
@@ -0,0 +1,10 @@
+package projektic;
+
+public class Adresa {
+	String broj,ulica,grad;
+	void unos(String broj, String ulica, String grad) {
+		this.broj=broj;
+		this.grad=grad;
+		this.ulica=ulica;
+	}
+}
