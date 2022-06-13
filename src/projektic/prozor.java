e6dc2c3a6d53ef62552155c742c5963d44c1c645
VukBabic <vukbabic1609@gmail.com>
2022-06-13 23:44:22 +0200

    FileMenu
    Dodavanje fajlova za tab menu.

--- src/projektic/prozor.java
+++ src/projektic/prozor.java
@@ -24,6 +24,13 @@ public class prozor{
 				window.setLocation(sirina/8, visina/8);
 				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			
+				
+				JTabbedPane tabbedPane = new JTabbedPane();
+				window.add(tabbedPane);
+				tabbedPane.setFocusable(false);
+				JLabel label=new JLabel("Placeholder");
+				
+				
 				JMenuBar menubar= new JMenuBar();
 				window.setJMenuBar(menubar);
 				//file bar
@@ -34,13 +41,37 @@ public class prozor{
 				JMenuItem pro_zap=new JMenuItem("Zaposleni");
 				JMenuItem pro_soft=new JMenuItem("Softver");
 				novi.add(pro_zap);
+				pro_zap.addActionListener(new ActionListener() {
+
+					public void actionPerformed(ActionEvent e) {
+						tabbedPane.addTab("Dodavanje Zaposlenih",label);
+					}
+				});
 				novi.add(pro_soft);
+				pro_soft.addActionListener(new ActionListener() {
+
+					public void actionPerformed(ActionEvent e) {
+						tabbedPane.addTab("Dodavanje Softvera",label);
+					}
+				});
 				file.add(novi);
 				JMenu open=new JMenu("Open");
 				JMenuItem tab_zap=new JMenuItem("Zaposleni");
 				JMenuItem tab_soft=new JMenuItem("Softver");
 				open.add(tab_zap);
+				tab_zap.addActionListener(new ActionListener() {
+
+					public void actionPerformed(ActionEvent e) {
+						tabbedPane.addTab("Tabela Zaposlenih",label);
+					}
+				});
 				open.add(tab_soft);
+				tab_soft.addActionListener(new ActionListener() {
+
+					public void actionPerformed(ActionEvent e) {
+						tabbedPane.addTab("Tabela Softvera",label);
+					}
+				});
 				file.add(open);
 				JMenuItem exit=new JMenuItem("Exit");
 				file.add(exit);
@@ -67,18 +98,18 @@ public class prozor{
 				JMenuItem about=new JMenuItem("About");
 				help.add(about);
 				
-				JTabbedPane tabbedPane = new JTabbedPane();
-				window.add(tabbedPane);
-				tabbedPane.setFocusable(false);
-				//tabbedPane.setPreferredSize(new Dimension(50,50));
 				
+				
+				/*
 				JLabel label1=new JLabel("egafgawfaaga");
 				JLabel label2=new JLabel("dfawfa");
 				
 				tabbedPane.add("Zaposleni",label1);
 				tabbedPane.add("Softver",label2);
+				*/
 				
 				window.setVisible(true);
+				
 				
 		}
 		static class zatvori implements ActionListener{
@@ -87,4 +118,3 @@ public class prozor{
 			}
 		}
 }
-
